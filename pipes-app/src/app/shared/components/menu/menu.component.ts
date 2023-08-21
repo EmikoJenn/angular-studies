3f61f89e8ce67a5fbddaa55ae86b625a577b5e2e
EmikoJenn <TabrisSoreth@gmail.com>
2023-08-21 14:09:25 -0600

    TS/ANGULAR/PIPES(setup): ADD PrimeNG

---
+++ pipes-app/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent {
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      { label: 'New', icon: 'pi pi-fw pi-plus' },
+      { label: 'Open', icon: 'pi pi-fw pi-download' },
+      { label: 'Undo', icon: 'pi pi-fw pi-refresh' },
+    ];
+  }
+}
